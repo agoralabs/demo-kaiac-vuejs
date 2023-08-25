9e03fa80b8ff84c9e8d6e15310f0025e3784d568
jekobokidou <joseph.ekobokidou@gmail.com>
2023-08-25 13:23:58 +0000

    Add kaiac Devops

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Login from '@/pages/Login.vue';
+import TheLogin from '@/pages/TheLogin.vue';
 import DashBoard from '@/pages/DashBoard.vue';
 import MyProjects from '@/pages/MyProjects.vue';
 import TheTeam from '@/pages/TheTeam.vue';
@@ -11,7 +11,7 @@ Vue.use(VueRouter);
 export default new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/', component: Login },
+        { path: '/', component: TheLogin },
         { path: '/dashboard', component: DashBoard },
         { path: '/projects', component: MyProjects },
         { path: '/team', component: TheTeam },
